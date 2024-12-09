474802ab0fc05983bea344aa76e27ed968e29324
JuanFranceschi <133984321+JuanFranceschi@users.noreply.github.com>
2024-12-09 18:45:27 -0300

    Car Integration

---
+++ android/app/src/main/kotlin/br/com/itatiaia/itatiaia_app/FlutterCarAppService.kt
@@ -0,0 +1,59 @@
+package br.com.itatiaia.itatiaia_app
+
+import android.content.Intent
+import androidx.car.app.*
+import androidx.car.app.model.Action
+import androidx.car.app.model.GridItem
+import androidx.car.app.model.GridTemplate
+import androidx.car.app.model.ItemList
+import androidx.car.app.model.ListTemplate
+import androidx.car.app.model.MessageTemplate
+import androidx.car.app.model.Template
+import androidx.car.app.validation.HostValidator
+import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
+
+class AndroidAutoSession : Session() {
+    override fun onCreateScreen(intent: Intent): Screen {
+        return MainScreen(carContext)
+    }
+}
+
+class MainScreen(carContext: CarContext) : Screen(carContext) {
+    companion object {
+         lateinit var screen:MainScreen
+         var counter: Int = 0
+    }
+    lateinit var template: Template
+    override fun onGetTemplate(): Template {
+        screen = this
+        screen.template = MessageTemplate.Builder("Counter: ${counter}")
+                .addAction(
+                        Action.Builder()
+                                .setTitle("Add")
+                                .setOnClickListener {
+                                    counter += 1
+                                    // AndroidAutoChannel.setCounter(counter)
+                                }
+                                .build())
+                .setHeaderAction(Action.APP_ICON)
+                .build()
+        return screen.template
+    }
+
+   fun updateCounter(value:Int) {
+       counter = value
+       invalidate()
+    }
+}
+
+class FlutterCarAppService  : CarAppService() {
+
+    override fun createHostValidator(): HostValidator {
+        return HostValidator.ALLOW_ALL_HOSTS_VALIDATOR
+    }
+
+    override fun onCreateSession(): Session {
+        return AndroidAutoSession()
+    }
+}
